ce67e696e66b1e7146d9d740e5a82ae2ef56ad63
alyssadsouza <alyssa.dsouza63@gmail.com>
2021-11-11 08:42:10 -0500

    Add sample code for ADC

--- moisture_readings.py
+++ moisture_readings.py
@@ -3,60 +3,99 @@
 import FakeRPi.GPIO as GPIO
 # On Pi, use this import:
 #import RPi.GPIO as GPIO
-import datetime
+import signal
+import sys
 import time
+import spidev
 
-init = False
+# Pin 15 on Raspberry Pi corresponds to GPIO 22
+LED1 = 15
+# Pin 16 on Raspberry Pi corresponds to GPIO 23
+LED2 = 16
 
-GPIO.setmode(GPIO.BOARD) # Broadcom pin-numbering scheme
+spi_ch = 0
 
-import datetime
-import time
+# Enable SPI
+spi = spidev.SpiDev(0, spi_ch)
+spi.max_speed_hz = 1200000
 
-init = False
+# to use Raspberry Pi board pin numbers
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
 
-GPIO.setmode(GPIO.BOARD) # Broadcom pin-numbering scheme
+# set up GPIO output channel
+GPIO.setup(LED1, GPIO.OUT)
+GPIO.setup(LED2, GPIO.OUT)
 
-def get_last_watered():
+def close(signal, frame):
+    GPIO.output(LED1, 0)
+    GPIO.output(LED2, 0)
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, close)
+
+def valmap(value, istart, istop, ostart, ostop):
+    value = ostart + (ostop - ostart) * ((value - istart) / (istop - istart))
+    if value > ostop:
+       value = ostop
+    return value
+
+def get_adc(channel):
+
+    # Make sure ADC channel is 0 or 1
+    if channel != 0:
+        channel = 1
+
+    # Construct SPI message
+    #  First bit (Start): Logic high (1)
+    #  Second bit (SGL/DIFF): 1 to select single mode
+    #  Third bit (ODD/SIGN): Select channel (0 or 1)
+    #  Fourth bit (MSFB): 0 for LSB first
+    #  Next 12 bits: 0 (don't care)
+    msg = 0b11
+    msg = ((msg << 1) + channel) << 5
+    msg = [msg, 0b00000000]
+    reply = spi.xfer2(msg)
+
+    # Construct single integer out of the reply (2 bytes)
+    adc = 0
+    for n in reply:
+        adc = (adc << 8) + n
+
+    # Last bit (0) is not part of ADC value, shift to remove it
+    adc = adc >> 1
+
+    # Calculate voltage form ADC value
+    # considering the soil moisture sensor is working at 5V
+    voltage = (5 * adc) / 1024
+
+    return voltage
+
+if __name__ == '__main__':
+    # Report the channel 0 and channel 1 voltages to the terminal
     try:
-        f = open("last_watered.txt", "r")
-        return f.readline()
-    except:
-        return "NEVER!"
-      
-def get_status(pin = 8):
-    GPIO.setup(pin, GPIO.IN) 
-    return GPIO.input(pin)
-
-def init_output(pin):
-    GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin, GPIO.LOW)
-    GPIO.output(pin, GPIO.HIGH)
-    
-def auto_water(delay = 5, pump_pin = 7, water_sensor_pin = 8):
-    consecutive_water_count = 0
-    init_output(pump_pin)
-    print("Here we go! Press CTRL+C to exit")
-    try:
-        while 1 and consecutive_water_count < 10:
-            time.sleep(delay)
-            wet = get_status(pin = water_sensor_pin) == 0
-            if not wet:
-                if consecutive_water_count < 5:
-                    pump_on(pump_pin, 1)
-                consecutive_water_count += 1
+        while True:
+            adc_0 = get_adc(0)
+            adc_1 = get_adc(1)
+            sensor1 = round(adc_0, 2)
+            if sensor1 < 0.5:
+                moisture1 = 0
             else:
-                consecutive_water_count = 0
-    except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
-        GPIO.cleanup() # cleanup all GPI
-
-def pump_on(pump_pin = 7, delay = 1):
-    init_output(pump_pin)
-    f = open("last_watered.txt", "w")
-    f.write("Last watered {}".format(datetime.datetime.now()))
-    f.close()
-    GPIO.output(pump_pin, GPIO.LOW)
-    time.sleep(1)
-    GPIO.output(pump_pin, GPIO.HIGH)
+                moisture1 = round(valmap(sensor1, 5, 3.5, 0, 100), 0)
+            sensor2 = round(adc_1, 2)
+            if sensor2 < 0.5:
+                moisture2 = 0
+            else:
+                moisture2 = round(valmap(sensor2, 5, 3.5, 0, 100), 0)
+            print("Soil Moisture Sensor 1:", moisture1, "% Soil Moisture Sensor 2:", moisture2, "%")
+            if moisture1 < 40 or moisture2 < 40:
+                GPIO.output(LED1, 1)
+                GPIO.output(LED2, 0)
+            else:
+                GPIO.output(LED1, 0)
+                GPIO.output(LED2, 1)
+            time.sleep(0.5)
+    finally:
+        GPIO.cleanup()
 
 print("working!")
